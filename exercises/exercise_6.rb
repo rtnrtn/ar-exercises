4d5c12ca44772e80dd8d70797c14f4fd59d5e38e
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-09-15 02:59:44 +0000

    Added one-to-many association between employees and stores.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ava", last_name: "Pirate", hourly_rate: 75)
+@store1.employees.create(first_name: "Zelda", last_name: "Tetris", hourly_rate: 100)
